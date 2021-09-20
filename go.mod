03f099bef216e93ed53a5110156fd6f06755bda3
Kyosuke Fujimoto <kyoro.f@gmail.com>
2021-09-20 16:37:17 +0900

    base

--- go.mod
+++ go.mod
@@ -1,3 +1,43 @@
 module github.com/lusingander/fynegit
 
 go 1.17
+
+require (
+	fyne.io/fyne/v2 v2.1.0
+	github.com/go-git/go-git/v5 v5.4.2
+	github.com/lusingander/gogigu v0.0.0-20210920065055-c6ba72de877b
+)
+
+require (
+	github.com/Microsoft/go-winio v0.5.0 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210707164159-52430bf6b52c // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/fredbi/uri v0.0.0-20181227131451-3dcfdacbaaf3 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/go-gl/gl v0.0.0-20210813123233-e4099ee2221f // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210410170116-ea3d685f79fb // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
+	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.1.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
+	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.1 // indirect
+	github.com/yuin/goldmark v1.3.8 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/image v0.0.0-20200430140353-33d19683fad8 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.3 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,103 @@
 package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/go-git/go-git/v5"
+	"github.com/lusingander/gogigu"
+)
+
+const (
+	appTitle = "fynegit"
+
+	dateTimeFormat   = "2006/01/02 15:04:05"
+	messageMaxLength = 80
+)
+
+func main() {
+	if err := run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(args []string) error {
+	path := args[1] // fixme
+	repo, err := openGitRepository(path)
+	if err != nil {
+		return err
+	}
+
+	a := app.New()
+	w := a.NewWindow(appTitle)
+	w.SetContent(buildCommitGraphView(repo))
+	w.Resize(fyne.NewSize(800, 600))
+	w.ShowAndRun()
+
+	return nil
+}
+
+func openGitRepository(path string) (*gogigu.Repository, error) {
+	src, err := git.PlainOpen(path)
+	if err != nil {
+		return nil, err
+	}
+	repo, err := gogigu.Calculate(src)
+	if err != nil {
+		return nil, err
+	}
+	return repo, nil
+}
+
+func buildCommitGraphView(repo *gogigu.Repository) fyne.CanvasObject {
+	return widget.NewList(
+		func() int {
+			return len(repo.Nodes)
+		},
+		func() fyne.CanvasObject {
+			return commitGraphItem()
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			updateCommitGraphItem(repo.Nodes[id], item)
+		},
+	)
+}
+
+func commitGraphItem() fyne.CanvasObject {
+	msg := widget.NewLabel("commit message")
+	hash := widget.NewLabel("hash")
+	committedAt := widget.NewLabel("2006/01/02 15:04:05")
+	return container.NewHBox(
+		msg,
+		hash,
+		committedAt,
+	)
+}
+
+func updateCommitGraphItem(node *gogigu.Node, item fyne.CanvasObject) {
+	objs := item.(*fyne.Container).Objects
+	objs[0].(*widget.Label).SetText(summaryMessage(node))
+	objs[1].(*widget.Label).SetText(shortHash(node))
+	objs[2].(*widget.Label).SetText(commitedAt(node))
+}
+
+func summaryMessage(node *gogigu.Node) string {
+	msg := strings.Split(node.Commit.Message, "\n")[0]
+	if len(msg) > messageMaxLength {
+		msg = msg[:messageMaxLength]
+	}
+	return msg
+}
+
+func shortHash(node *gogigu.Node) string {
+	return node.Hash[:7]
+}
+
+func commitedAt(node *gogigu.Node) string {
+	return node.Commit.Committer.When.Format(dateTimeFormat)
+}
